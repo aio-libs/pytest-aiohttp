d39ba03e5914c0537c9bba72d2776c29fafea95c
Sam Bull <git@sambull.org>
2024-12-02 14:32:15 +0000

    Export types at top-level

--- pytest_aiohttp/__init__.py
+++ pytest_aiohttp/__init__.py
@@ -1,1 +1,6 @@
 from ._version import version as __version__  # noqa
+from .plugin import (
+    AiohttpClient as AiohttpClient,
+    AiohttpRawServer as AiohttpRawServer,
+    AiohttpServer as AiohttpServer,
+)
