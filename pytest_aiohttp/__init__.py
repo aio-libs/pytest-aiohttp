168332f79596f523c28f2e377ed4393351032181
Sam Bull <git@sambull.org>
2024-12-02 15:02:44 +0000

    Update __init__.py

--- pytest_aiohttp/__init__.py
+++ pytest_aiohttp/__init__.py
@@ -1,5 +1,5 @@
 from ._version import version as __version__  # noqa: F401
-from .plugin import (
+from .plugin import (  # noqa: F401
     AiohttpClient as AiohttpClient,
     AiohttpRawServer as AiohttpRawServer,
     AiohttpServer as AiohttpServer,
